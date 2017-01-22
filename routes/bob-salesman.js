eb8024a5936feb5e8bf763ca1ade553426050b20
Leonardo Jose  Schettini de Arruda <a1601323@unet.univie.ac.at>
2017-01-22 23:10:36 +0100

    testing

--- routes/bob-salesman.js
+++ routes/bob-salesman.js
@@ -13,26 +13,22 @@ router.get('/', function(req, res) {
 function run(requesterID, callback, progress = 0) {
 	console.log('running bob for execution %d', requesterID);
 	
-	console.log('sleep(10000) + progress call');
 	sleep(10000);
 
 	request({
-		uri: 'http://localhost:5000/bob-salesman/',
+		uri: 'https://bob-salesman.herokuapp.com/bob-salesman/',
 		method: 'GET',
 		json: { progress: progress }
 
 	}, function (error, response, body) {
-		console.log('Response to GET method, made to bob-salesman, received.')
 		if (!error && response.statusCode == 200) {
 			var curProgress = body.progress;
 			console.log('Successfully GET: ' + curProgress);
 
 			if (curProgress < 100) {
-				console.log('< 100')
 				callback(requesterID, 'progress: ' + curProgress);
 				run(requesterID, callback, curProgress);
 			} else {
-				console.log('else')
 				callback(requesterID, 'this will be the URL for answer file');
 			}
 		} else {
